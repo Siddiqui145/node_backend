ec09a0a92d92ccee8d9f42f49608488ec3696746
Siddiqui Taha <tahasiddiqui541@gmail.com>
2025-03-22 15:52:35 +0530

    env modified & used

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const port = 3000
@@ -18,6 +20,6 @@ app.get('/youtube', (req, res) => {
     res.send('<h2> Welcome to Youtube </h2>')
 })
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
 })
